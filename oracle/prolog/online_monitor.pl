b92ea249415a08b1f6e717011f78ef4b8a20f14b
angelo <angelo.ferrando42@gmail.com>
2019-07-29 14:25:18 +0100

    side filed in configuration file for the generator

--- oracle/prolog/online_monitor.pl
+++ oracle/prolog/online_monitor.pl
@@ -1,26 +1,3 @@
-% MIT License
-%
-% Copyright (c) [2019] [Davide Ancona, Luca Franceschini, Angelo Ferrando, Viviana Mascardi]
-%
-% Permission is hereby granted, free of charge, to any person obtaining a copy
-% of this software and associated documentation files (the "Software"), to deal
-% in the Software without restriction, including without limitation the rights
-% to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-% copies of the Software, and to permit persons to whom the Software is
-% furnished to do so, subject to the following conditions:
-%
-% The above copyright notice and this permission notice shall be included in all
-% copies or substantial portions of the Software.
-%
-% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-% OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-% SOFTWARE.
-
-
 :- use_module(library(http/websocket)).
 :- use_module(library(http/thread_httpd)).
 :- use_module(library(http/http_dispatch)).
@@ -48,7 +25,7 @@
 
 % load specification
 
-:- current_prolog_flag(argv, [Spec|_]), use_module(Spec).
+:- current_prolog_flag(argv, [Spec|L]), L = [Port|_], asserta(port(Port)), use_module(Spec).
 
 server(Port) :- http_server(http_dispatch,[port('127.0.0.1':Port),workers(1)]). %% one worker to guarantee event sequentiality
 %server(Port) :- http_server(http_dispatch,[port('192.168.1.250':Port),workers(1)]). %% one worker to guarantee event sequentiality
@@ -57,25 +34,22 @@ log(Log) :-
     nb_getval(log,Stream), Stream\==null->  %% optional logging of server activity
 	(Log=(TE,E)->
 	     writeln(Stream,"Trace expression:"),writeln(Stream,TE),writeln(Stream,"Event: "),writeln(Stream,E);
-	 writeln(Stream,"Error")),
+	 Log=(TE,E,error), writeln(Stream,"Trace expression:"),writeln(Stream,TE),writeln(Stream,"Event: "),writeln(Stream,E),writeln(Stream,"Error")),
 	nl(Stream),
 	flush_output(Stream);
     true.
 
-why(Clauses) :- findall((ET, Dict, Clause), clause(match(Ev, ET), (deep_subdict(Dict, Ev), Clause)), Clauses).
-
 manage_event(WebSocket) :-
     ws_receive(WebSocket, Msg, [format(json),value_string_as(string)]), %% value_string_as(atom) passed as option to json_read_dict/3
-    writeln('Monitor has observed: '),
-    writeln(Msg),
+    %writeln('Monitor has observed: '),
+    %writeln(Msg),
     (Msg.opcode==close ->
 	     true;
 	       E=Msg.data,
-         writeln('Message data: '),
-         writeln(E),
+         %writeln('Message data: '),
+         %writeln(E),
 	       nb_getval(state,TE1),
-	       log((TE1,E)),
-         (next(TE1,E,TE2) -> nb_setval(state,TE2),Reply=E; term_string(TE1, TE1Str), Reply=(_{}.put(E).put(_{error:true, spec:TE1Str}))),
+	       (next(TE1,E,TE2) -> log((TE1,E)), nb_setval(state,TE2),Reply=E; log((TE1,E,error)), Reply=(_{}.put(E).put(_{error:true}))),
 	       atom_json_dict(Json,Reply,[as(string)]),
 	       ws_send(WebSocket, string(Json)),
 	       manage_event(WebSocket)).
@@ -84,4 +58,4 @@ manage_event(WebSocket) :-
 exception(undefined_global_variable, state, retry) :- trace_expression(_, TE), nb_setval(state,TE).
 exception(undefined_global_variable, log, retry) :- (current_prolog_flag(argv, [_,LogFile|_])->open(LogFile,append,Stream);Stream=null),nb_setval(log, Stream).
 
-:- server(8080).
+:- port(Port), writeln(Port), atom_number(Port, P), server(P).
